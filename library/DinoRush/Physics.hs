68826de7a220dea655d8def15faa27f5e705b1c2
jxv <joevargas92@gmail.com>
2018-02-08 10:08:32 -0800

    rename collision module to physics

---
+++ library/DinoRush/Physics.hs
@@ -0,0 +1,11 @@
+module DinoRush.Physics where
+
+import Linear.V2
+
+data Aabb = Aabb
+  { aMin :: V2 Float
+  , aMax :: V2 Float
+  }
+
+collisionIntersect :: Aabb -> Aabb -> Bool
+collisionIntersect (Aabb (V2 al au) (V2 ar ad)) (Aabb (V2 bl bu) (V2 br bd)) = ar >= bl && al <= br && ad >= bu && au <= bd
